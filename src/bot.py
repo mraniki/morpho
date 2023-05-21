ce838e4df18bd73b935dae89f60d654719d1f4ba
mraniki <8766259+mraniki@users.noreply.github.com>
2023-05-21 12:36:36 +0200

    🔍 better error handling in case of not correct text

--- src/bot.py
+++ src/bot.py
@@ -24,52 +24,55 @@ def data_selector():
         text_value = worksheet.acell(f'E{i}').value
         text_status = worksheet.acell(f'F{i}').value
         if text_status != 'Done':
-            break
-    logger.info("Data to submit: %s", text_value)
+            if text_value != '#ERROR!':
+                break
+    logger.info("Text selected: %s", text_value)
     return text_value
 
 
 def data_update():
     cell = worksheet.find(text_value)
-    logger.debug(cell)
-    logger.debug(cell.address)
     worksheet.update(f'F{cell.address[1:]}', 'Done')
+    logger.info("Spreasheet updated")
 
 
 async def navigator():
-    while True:
-        async with async_playwright() as playwright:
-            chromium = playwright.chromium
-            browser = await chromium.launch()
-            page = await browser.new_page()
-            await page.goto(settings.url)
-            element = await page.query_selector(settings.check_identifier)
-            check = await page.evaluate(
-                '(element) => element.textContent',
-                element)
-            logger.debug(check)
-            data = re.findall(r'\d+', check)
-            logger.info(data)
-            data_process = int(data[0]) + int(data[1])
-            logger.info("Control: %s", data_process)
-            await page.keyboard.type(data_selector())
-            await page.click(settings.check_selector)
-            await page.keyboard.type(f"{data_process}")
-            await page.click(settings.selector1)
-            await page.click(settings.selector2)
-            await page.screenshot(path="loaded.png", full_page=True)
-            await asyncio.sleep(5)
-            if settings.activeflag == "True":
-                logger.info("Submitting")
-                await page.click(settings.selector3)
+    try:
+        while True:
+            async with async_playwright() as playwright:
+                chromium = playwright.chromium
+                browser = await chromium.launch()
+                page = await browser.new_page()
+                await page.goto(settings.url)
+                element = await page.query_selector(settings.check_identifier)
+                check = await page.evaluate(
+                    '(element) => element.textContent',
+                    element)
+                logger.debug(check)
+                data = re.findall(r'\d+', check)
+                logger.info(data)
+                data_process = int(data[0]) + int(data[1])
+                logger.info("Control: %s", data_process)
+                await page.keyboard.type(data_selector())
+                await page.click(settings.check_selector)
+                await page.keyboard.type(f"{data_process}")
+                await page.click(settings.selector1)
+                await page.click(settings.selector2)
+                await page.screenshot(path="loaded.png", full_page=True)
                 await asyncio.sleep(5)
-                await page.screenshot(path="success.png", full_page=True)
-                data_update()
-                logger.info("Submit Done")
-            await browser.close()
-            sleep = random.randint(72, 3606)
-            logger.info("sleeping: %s", sleep)
-            await asyncio.sleep(sleep)
+                if settings.activeflag == "True":
+                    logger.info("Submitting")
+                    await page.click(settings.selector3)
+                    await asyncio.sleep(5)
+                    await page.screenshot(path="success.png", full_page=True)
+                    data_update()
+                    logger.info("Submit Done")
+                await browser.close()
+                sleep = random.randint(72, 3606)
+                logger.info("sleeping: %s", sleep)
+                await asyncio.sleep(sleep)
+    except Exception as e:
+        logger.error("Bot issue: %s", e)
 
 # ⛓️API
 app = FastAPI(title="navigator",)
