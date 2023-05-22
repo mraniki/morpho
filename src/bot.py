31e45789c76294fde677cfb0d7a16961566c24bb
mraniki <8766259+mraniki@users.noreply.github.com>
2023-05-22 09:40:26 +0200

    ⚡ sleeping params

--- src/bot.py
+++ src/bot.py
@@ -75,7 +75,7 @@ async def navigator():
                     logger.info("skipping submission")
             except Exception as e:
                 logger.error("Bot issue: %s", e)
-            sleep = random.randint(72, 1200)
+            sleep = random.randint(30, settings.sleeping)
             logger.info("sleeping: %s", sleep)
             await asyncio.sleep(sleep)
 
