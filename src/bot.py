babd30b7c9aa8c50f54bf8d99d09802b41f21c8f
mraniki <mraniki@users.noreply.github.com>
2023-05-23 08:51:27 +0200

    :zap: settings

--- src/bot.py
+++ src/bot.py
@@ -75,7 +75,7 @@ async def navigator():
                     logger.info("skipping submission")
             except Exception as e:
                 logger.error("Bot issue: %s", e)
-            sleep = random.randint(30, settings.sleeping)
+            sleep = random.randint(3000, settings.sleeping)
             logger.info("sleeping: %s", sleep)
             await asyncio.sleep(sleep)
 
